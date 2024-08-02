a7477c6b964280690948ec6e569d8a540487c2c0
abdulsalam11-alao <abdulsalamalao89@gmail.com>
2024-08-02 04:08:32 +0100

    Red-card

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,11 +1,11 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../service/apiRestaurant";
-import MenuItem from "../menu/MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../service/apiRestaurant';
+import MenuItem from '../menu/MenuItem';
 function Menu() {
   const menu = useLoaderData();
   console.log(menu);
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza}></MenuItem>
       ))}
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,25 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const className =
-    'text uppercase-stone-800 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+function Button({ children, disabled, to, type }) {
+  const base =
+    'text uppercase-stone-800 text-sm inline-block rounded-full bg-yellow-400 font-semibold tracking-wide transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
+
+  const style = {
+    primary: base + ' px-4 py-3  md:px-6 md:py-4',
+    small: base + ' px-4 py-2  md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'text uppercase-stone-800 text-sm px-4 py-3  md:px-6 md:py-3.5 inline-block rounded-full  text-stone-400 border-2 border-stone-300 font-semibold tracking-wide transition-colors duration-300 hover:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 hover:text-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed ',
+  };
   if (to) {
     return (
-      <Link className={className} to="/order/new">
+      <Link className={style[type]} to="/order/new">
         Order pizzas
       </Link>
     );
   }
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={style[type]}>
       {children}
     </button>
   );
@@ -22,6 +29,7 @@ Button.propTypes = {
   children: PropTypes.string,
   disabled: PropTypes.bool,
   to: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default Button;
